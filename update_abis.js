5402800a4ddd35c35e80a072e2772cee32b214b1
Guillaume Felley <guillaume@felley.io>
2020-10-12 11:15:59 +0200

    Update abis for 0.9.0

--- update_abis.js
+++ update_abis.js
@@ -2,54 +2,49 @@ const fs = require('fs')
 const exec = require('child_process').exec
 const _resolve = require('path').resolve
 const axios = require('axios')
+const prettier = require('prettier')
 
 // Update ABI the folder by fetching the new ABIs from changelog repo
 // Use: node update_abis.js <path to abi folder> <version ig: kovan/0.6.0>
-
 const path = resolve(process.argv[2])
-const gebVersion = process.argv[3] || 'kovan/0.6.0'
-
-console.log(`Looking for ABIs in ${path}`)
-function httpGet(url) {
-  return new Promise(function(resolve, reject) {
-    axios.get(url).then(x => resolve(x.data))
-  })
-}
-
-async function sh(cmd) {
-  return new Promise(function(resolve, reject) {
-    exec(cmd, (err, stdout, stderr) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve({ stdout, stderr })
-      }
-    })
-  })
-}
+const gebVersion = process.argv[3] || 'kovan/0.9.0'
 
 function resolve(...args) {
-  args = args.slice()
-  args.unshift('..')
-  args.unshift(__dirname)
-  return _resolve.apply(null, args)
+    args = args.slice()
+    args.unshift('..')
+    args.unshift(__dirname)
+    return _resolve.apply(null, args)
 }
 
 async function main() {
-  let files = fs.readdirSync(path)
-  files = files.filter(x => x.endsWith('.json'))
-  for (let f of files) {
-    const abi = await httpGet(
-      `https://raw.githubusercontent.com/reflexer-labs/geb-changelog/master/releases/${gebVersion}/median/fixed-discount/abi/${f.slice(
-        0,
-        -5,
-      )}.abi`,
-    )
-    fs.writeFileSync(path + '/' + f, JSON.stringify(abi))
-  }
-  console.log(`prettier -w ${path}/*.json`)
-  
-  await sh(`prettier -w ${path}/*.json`)
+    let files = fs.readdirSync(path)
+    let prettierConfig = await prettier.resolveConfig('.prettierrc.json')
+
+    files = files.filter((x) => x.endsWith('.json'))
+    for (let f of files) {
+        const url = `https://raw.githubusercontent.com/reflexer-labs/geb-changelog/master/releases/${gebVersion}/median/fixed-discount/abi/${f.slice(
+            0,
+            -5
+        )}.abi`
+
+        let abi
+        try {
+            abi = (await axios.get(url)).data
+        } catch (err) {
+            if (err.response.status === 404) {
+                console.log(`- ABI ${f} not found at url ${url}`)
+                continue
+            } else {
+                throw Error('Error fetching ABIs')
+            }
+        }
+
+        // Stringify and format abi
+        abi = JSON.stringify(abi)
+        abi = prettier.format(abi, { ...prettierConfig, parser: 'json' })
+
+        fs.writeFileSync(path + '/' + f, abi)
+    }
 }
 
 main()

21a35e23f1c4f13b208083a099f183ad0baee1ed
Guillaume Felley <guillaume@felley.io>
2020-11-23 17:50:11 +0100

    Added perSafeDebtCeilling

--- src/utils/integer.ts
+++ src/utils/integer.ts
@@ -1,7 +1,10 @@
-import { BigInt } from '@graphprotocol/graph-ts'
+import { BigInt, Bytes } from '@graphprotocol/graph-ts'
 
 export let ONE = BigInt.fromI32(1)
 export let ZERO = BigInt.fromI32(0)
+export let MAX_UINT_256 = BigInt.fromUnsignedBytes(
+  Bytes.fromHexString('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff') as Bytes,
+)
 export let HOUR = BigInt.fromI32(60).times(BigInt.fromI32(60))
 export let DAY = HOUR.times(BigInt.fromI32(24))
 export let SECOND_PER_YEAR = BigInt.fromI32(31536000)

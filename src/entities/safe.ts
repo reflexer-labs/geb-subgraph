ad8f81aab20324297b942ad71b83589e5fac2d66
Guillaume Felley <guillaume@felley.io>
2020-11-24 16:18:10 +0100

    Fixing the previous commit (balance references)

--- src/entities/safe.ts
+++ src/entities/safe.ts
@@ -96,7 +96,7 @@ function createSafe(safeHandler: Bytes, collateral: Bytes, event: ethereum.Event
   safe.collateral = decimal.ZERO
   safe.debt = decimal.ZERO
   safe.safeHandler = safeHandler
-  safe.internalCollateralBalances = collateral.toHexString()
+  safe.internalCollateralBalances = safeHandler.toHexString() + "-" + collateral.toString()
   safe.internalCoinBalance = safeHandler.toHexString()
   safe.createdAt = event.block.timestamp
   safe.createdAtBlock = event.block.number
